9af8523801d9d387625741800db30a01e48da970
Umit <i.umitakgun@gmail.com>
2016-04-05 16:46:24 -0400

    update

--- build/PointerStructureAnalysis/CMakeFiles/PointerStructureAnalysis.dir/DependInfo.cmake
+++ build/PointerStructureAnalysis/CMakeFiles/PointerStructureAnalysis.dir/DependInfo.cmake
@@ -4,24 +4,24 @@ set(CMAKE_DEPENDS_LANGUAGES
   )
 # The set of files for implicit dependencies of each language:
 set(CMAKE_DEPENDS_CHECK_CXX
-  "/Users/umit/research/llvmDataStructureAnalyzer/PointerStructureAnalysis/PointerStructureAnalysis.cpp" "/Users/umit/research/llvmDataStructureAnalyzer/build/PointerStructureAnalysis/CMakeFiles/PointerStructureAnalysis.dir/PointerStructureAnalysis.cpp.o"
+  "/home/umit/research/llvmDataStructureAnalyzer/PointerStructureAnalysis/PointerStructureAnalysis.cpp" "/home/umit/research/llvmDataStructureAnalyzer/build/PointerStructureAnalysis/CMakeFiles/PointerStructureAnalysis.dir/PointerStructureAnalysis.cpp.o"
   )
-set(CMAKE_CXX_COMPILER_ID "Clang")
+set(CMAKE_CXX_COMPILER_ID "GNU")
 
 # Preprocessor definitions for this target.
-set(CMAKE_TARGET_DEFINITIONS_CXX
+set(CMAKE_TARGET_DEFINITIONS
   "__STDC_CONSTANT_MACROS"
   "__STDC_LIMIT_MACROS"
-  )
-
-# The include file search paths:
-set(CMAKE_CXX_TARGET_INCLUDE_PATH
-  "/usr/local/include"
   )
 
 # Targets to which this target links.
 set(CMAKE_TARGET_LINKED_INFO_FILES
   )
 
-# Fortran module output directory.
-set(CMAKE_Fortran_TARGET_MODULE_DIR "")
+# The include file search paths:
+set(CMAKE_C_TARGET_INCLUDE_PATH
+  "/usr/lib/llvm-3.6/include"
+  )
+set(CMAKE_CXX_TARGET_INCLUDE_PATH ${CMAKE_C_TARGET_INCLUDE_PATH})
+set(CMAKE_Fortran_TARGET_INCLUDE_PATH ${CMAKE_C_TARGET_INCLUDE_PATH})
+set(CMAKE_ASM_TARGET_INCLUDE_PATH ${CMAKE_C_TARGET_INCLUDE_PATH})
