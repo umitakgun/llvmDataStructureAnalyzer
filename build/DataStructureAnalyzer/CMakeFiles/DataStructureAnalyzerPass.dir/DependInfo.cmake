4e92be01d4821c843ef1a376930030dd6ccc5401
umit <i.umitakgun@gmail.com>
2016-09-16 22:22:29 +0300

    Osx updates

--- build/DataStructureAnalyzer/CMakeFiles/DataStructureAnalyzerPass.dir/DependInfo.cmake
+++ build/DataStructureAnalyzer/CMakeFiles/DataStructureAnalyzerPass.dir/DependInfo.cmake
@@ -4,15 +4,19 @@ set(CMAKE_DEPENDS_LANGUAGES
   )
 # The set of files for implicit dependencies of each language:
 set(CMAKE_DEPENDS_CHECK_CXX
-  "/home/umit/research/llvmDataStructureAnalyzer/DataStructureAnalyzer/CGAnalyzer.cpp" "/home/umit/research/llvmDataStructureAnalyzer/build/DataStructureAnalyzer/CMakeFiles/DataStructureAnalyzerPass.dir/CGAnalyzer.cpp.o"
-  "/home/umit/research/llvmDataStructureAnalyzer/DataStructureAnalyzer/DataStructureAnalyzer.cpp" "/home/umit/research/llvmDataStructureAnalyzer/build/DataStructureAnalyzer/CMakeFiles/DataStructureAnalyzerPass.dir/DataStructureAnalyzer.cpp.o"
+  "/Users/umit/research/llvmDataStructureAnalyzer/DataStructureAnalyzer/CGAnalyzer.cpp" "/Users/umit/research/llvmDataStructureAnalyzer/build/DataStructureAnalyzer/CMakeFiles/DataStructureAnalyzerPass.dir/CGAnalyzer.cpp.o"
+  "/Users/umit/research/llvmDataStructureAnalyzer/DataStructureAnalyzer/DataStructureAnalyzer.cpp" "/Users/umit/research/llvmDataStructureAnalyzer/build/DataStructureAnalyzer/CMakeFiles/DataStructureAnalyzerPass.dir/DataStructureAnalyzer.cpp.o"
   )
-set(CMAKE_CXX_COMPILER_ID "GNU")
+set(CMAKE_CXX_COMPILER_ID "Clang")
 
 # The include file search paths:
 set(CMAKE_CXX_TARGET_INCLUDE_PATH
-  "/home/umit/research/llvm/src_390/include"
-  "/home/umit/research/llvm/build_390/include"
+  "../[BEFORE]"
+  "../[SYSTEM]"
+  "/Applications/Xcode.app/Contents/Developer/Toolchains/XcodeDefault.xctoolchain/usr/bin/../include/c++/v1"
+  "/Applications/Xcode.app/Contents/Developer/Toolchains/XcodeDefault.xctoolchain/usr/include"
+  "/Users/umit/research/llvm/src_390/include"
+  "/Users/umit/research/llvm/build_390/include"
   )
 
 # Targets to which this target links.
--- build/PointerStructureAnalysis/CMakeFiles/PointerStructureAnalysis.dir/DependInfo.cmake
+++ build/PointerStructureAnalysis/CMakeFiles/PointerStructureAnalysis.dir/DependInfo.cmake
@@ -4,14 +4,18 @@ set(CMAKE_DEPENDS_LANGUAGES
   )
 # The set of files for implicit dependencies of each language:
 set(CMAKE_DEPENDS_CHECK_CXX
-  "/home/umit/research/llvmDataStructureAnalyzer/PointerStructureAnalysis/PointerStructureAnalysis.cpp" "/home/umit/research/llvmDataStructureAnalyzer/build/PointerStructureAnalysis/CMakeFiles/PointerStructureAnalysis.dir/PointerStructureAnalysis.cpp.o"
+  "/Users/umit/research/llvmDataStructureAnalyzer/PointerStructureAnalysis/PointerStructureAnalysis.cpp" "/Users/umit/research/llvmDataStructureAnalyzer/build/PointerStructureAnalysis/CMakeFiles/PointerStructureAnalysis.dir/PointerStructureAnalysis.cpp.o"
   )
-set(CMAKE_CXX_COMPILER_ID "GNU")
+set(CMAKE_CXX_COMPILER_ID "Clang")
 
 # The include file search paths:
 set(CMAKE_CXX_TARGET_INCLUDE_PATH
-  "/home/umit/research/llvm/src_390/include"
-  "/home/umit/research/llvm/build_390/include"
+  "../[BEFORE]"
+  "../[SYSTEM]"
+  "/Applications/Xcode.app/Contents/Developer/Toolchains/XcodeDefault.xctoolchain/usr/bin/../include/c++/v1"
+  "/Applications/Xcode.app/Contents/Developer/Toolchains/XcodeDefault.xctoolchain/usr/include"
+  "/Users/umit/research/llvm/src_390/include"
+  "/Users/umit/research/llvm/build_390/include"
   )
 
 # Targets to which this target links.
